813d480321406438acd97a73a07f28bad5836c45
Hansee <hansee1996@gmail.com>
2023-05-07 14:50:11 -0500

    Correciones colores

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,10 @@ template.innerHTML =
     `<div class="list-header">
     <ul>
         <li><a id="index" href="../../index.html" onClick="pageReference('index')">Home</a><fa-icon [icon]="faAddressCard"></fa-icon></li>
-        <li><a id="java" href="/app/pages/java/java.html" onClick="pageReference('java')">Java</a><fa-icon [icon]="faGraduation"></fa-icon></li>
-        <li><a id="python" href="/app/pages/python/python.html" onClick="pageReference('python')">Python</a><fa-icon [icon]="faTools"></fa-icon></li>
-        <li><a id="javascript" href="/app/pages/js/js.html" onClick="pageReference('js')">JavaScript</a><fa-icon [icon]="faJob"></fa-icon></li>
-        <li><a id="cplusplus" href="/app/pages/cplusplus/cplusplus.html" onClick="pageReference('cplusplus')">C++</a><fa-icon [icon]="faCurriculum"></fa-icon></li>
+        <li><a id="java" href="/pages/java/java.html" onClick="pageReference('java')">Java</a><fa-icon [icon]="faGraduation"></fa-icon></li>
+        <li><a id="python" href="/pages/python/python.html" onClick="pageReference('python')">Python</a><fa-icon [icon]="faTools"></fa-icon></li>
+        <li><a id="js" href="/pages/js/js.html" onClick="pageReference('js')">JavaScript</a><fa-icon [icon]="faJob"></fa-icon></li>
+        <li><a id="cplusplus" href="/pages/cplusplus/cplusplus.html" onClick="pageReference('cplusplus')">C++</a><fa-icon [icon]="faCurriculum"></fa-icon></li>
     </ul>
 </div>`
 
@@ -22,7 +22,8 @@ customElements.define("menu-principal", Menu);
 const pageReference = (pageName) => {
     let ref = window.location.href
     if (ref.includes(pageName)) {
+        console.log("si")
         let linkElement = document.querySelector("#" + pageName)
-        linkElement.setAttribute("href", "#about-me")
+        linkElement.setAttribute("href", pageName === "index" ? "#languages":"#description")
     }
 }
